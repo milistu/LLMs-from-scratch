e302e42534402f82b877037e08095049f493d14e
milistu <81291567+milistu@users.noreply.github.com>
2026-06-16 12:36:01 +0100

    feat: add script with multihead attention inmplementation

---
+++ ch03/01_main-chapter-code/multihead_attention.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self,
+        d_in: int,
+        d_out: int,
+        context_length: int,
+        dropout: float,
+        num_heads: int,
+        qkv_bias: bool = False,
+    ) -> None:
+        super().__init__()
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        # Split the matrix into multiple heads
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose to align dimensions that we need for further calculation
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
+
+        return context_vec
+
+
+if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    dataloader_dir = "ch02/01_main-chapter-code"
+    sys.path.insert(0, str(dataloader_dir))
+
+    from dataloader import create_dataloader_v1  # pyright: ignore[reportMissingImports]
+
+    file_path = Path("ch02/01_main-chapter-code/the-verdict.txt")
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_text = f.read()
+
+    vocab_size = 50_257
+    output_dim = 256
+    context_length = 1024
+
+    token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+    pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+
+    batch_size = 8
+    max_length = 4
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=batch_size, max_length=max_length, stride=max_length
+    )
+
+    data_iter = iter(dataloader)
+    x, y = next(data_iter)
+
+    token_embeddings = token_embedding_layer(x)
+    pos_embeddings = pos_embedding_layer(torch.arange(max_length))
+
+    input_embeddings = token_embeddings + pos_embeddings
+
+    print(f"Input embeddings shape: {input_embeddings.shape}")
+
+    torch.manual_seed(123)
+
+    d_in = output_dim
+    d_out = d_in
+
+    mha = MultiHeadAttention(d_in, d_out, max_length, 0.0, num_heads=2)
+
+    batch = input_embeddings
+    context_vecs = mha(batch)
+
+    print(f"Context vector shape: {context_vecs.shape}")
